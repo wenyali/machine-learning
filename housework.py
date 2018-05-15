bbfec72897a9681a30d2ff830bb3a728d7c5e2b1
wenyali <2917073217@qq.com>
2018-05-15 16:05:23 +0800

    all

---
+++ housework.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_coding:utf-8 _*_
+
+import numpy as np
+
+__title__ = ''
+__author__ = "wenyali"
+__mtime__ = "2018/5/15"
+
+np.random.seed(0)
+data = np.random.rand(40,1)
+print(data)
